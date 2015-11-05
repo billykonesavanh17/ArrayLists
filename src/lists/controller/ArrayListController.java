5d55788d810884d899a3d7329f538481cee6d530
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-11-05 08:03:50 -0700

    Added a controller.

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,11 +1,30 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
-
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("jadlkfjdfjoaldskjf u");
+		firstWords.add("mumumumumum");
+		showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot# " + spot);
+		}
 	}
 	
 }
